ae82c8d01e047877c7c36b1569b31085148da1a3
Maxime Maheo <mmaheo@deezer.com>
2020-09-30 10:32:08 +0200

    Create project

---
+++ PharmApp/PharmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharmAppApp.swift
+//  PharmApp
+//
+//  Created by Maxime Maheo on 30/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct PharmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
